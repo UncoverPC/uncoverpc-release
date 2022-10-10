66f57f6e136c471112e139d9b0e2a4be6c80ff49
Jonathan Yuan <jy90023@gmail.com>
2022-10-09 23:20:23 -0400

    make login pages responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
+        screens: {
+            xs: '480px',
+            ...defaultTheme.screens,
+        },
         extend: {},
     },
     plugins: [],
