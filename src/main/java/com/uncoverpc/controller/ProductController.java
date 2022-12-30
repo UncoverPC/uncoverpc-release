5379530dd7d229972a80b785ee9f156950e11581
AlexanderL <al.x.300000@gmail.com>
2022-12-29 20:21:46 -0500

    search query controller

--- src/main/java/com/uncoverpc/controller/ProductController.java
+++ src/main/java/com/uncoverpc/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.uncoverpc.controller;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,5 +19,5 @@ public class ProductController {
 	public ModelAndView addProduct() {
 		return new ModelAndView("/add_product.html");
 	}
-	
+
 }
--- src/main/java/com/uncoverpc/utils/Utils.java
+++ src/main/java/com/uncoverpc/utils/Utils.java
@@ -1,33 +1,19 @@
-package com.uncoverpc.controller;
+package com.uncoverpc.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.uncoverpc.db.QuizService;
-import com.uncoverpc.model.quiz.Quiz;
-
-@Controller("api")
-public class SearchController {
-
-	@Autowired
-	QuizService quizService;
+public class Utils {
+	
 	
 	@PostMapping("/search")
-	public List<Quiz> getQuiz(@RequestBody String quiz){
-		//TO DO get matched quizzes
-		return quizService.findAll();
-	}
-	@PostMapping("/searchProduct")
 	public Map<String, Object> processSearch (@RequestBody String search) {
-//		search = "Laptop, Large Screen, Black";
+		search = "Laptop, Large Screen, Black";
 		List<String> list = Arrays.asList(search.split(","));
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("Product Type", list.get(0));
@@ -36,5 +22,4 @@ public class SearchController {
 //		map.forEach((key, value) -> System.out.println(key + ":" + value));
 		return map;
 	}
-	
 }
